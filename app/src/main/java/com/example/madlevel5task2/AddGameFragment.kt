e74ec95bcabcd1053804d9f59a5d3b031baad76c
Oscar Mollé <scarmoll@gmail.com>
2020-12-12 18:00:04 +0100

    add game fragment populated

--- app/src/main/java/com/example/madlevel5task2/AddGameFragment.kt
+++ app/src/main/java/com/example/madlevel5task2/AddGameFragment.kt
@@ -6,16 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_add_game.*
+import java.util.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class AddGameFragment : Fragment() {
 
+    private val viewModel: GameViewModel by viewModels()
+
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_game, container, false)
@@ -24,8 +29,18 @@ class AddGameFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-//            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-//        }
+        fabSave.setOnClickListener { saveGame() }
+    }
+
+    private fun saveGame() {
+        val title = tiTitle.text.toString()
+        val platform = tiPlatform.text.toString()
+        val date = Date(
+            Integer.parseInt(tiYear.text.toString()),
+            Integer.parseInt(tiMonth.text.toString()),
+            Integer.parseInt(tiDay.text.toString())
+        )
+
+        viewModel.insertGame(title, platform, date)
     }
 }
